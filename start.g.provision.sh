bffb62cb6ba3639e65eaa63f85df46c12bbfde70
vavan11 <vovamelnik@gmail.com>
2017-06-07 14:06:07 +0300

    add start options

--- start.g.provision.sh
+++ start.g.provision.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+gcloud compute instances create confluence --image-project ubuntu-os-cloud --image-family ubuntu-1604-lts --boot-disk-size 20GB --can-ip-forward --machine-type n1-standard-2 --description confluence --address 130.211.69.83
+sleep 30 && gcloud compute ssh vovamelnik@confluence --ssh-key-file ~/.ssh/id_rsa --plain
 ssh-keygen -f "/home/metaxa/.ssh/known_hosts" -R 130.211.69.83
-scp provision.g.cloud.sh ssh Google:~
+sleep 2 && scp provision.g.cloud.sh ssh Google:~
 scp server.xml ssh Google:~
 scp default ssh Google:~
 ssh Google 'chmod +x ~/provision.g.cloud.sh'
